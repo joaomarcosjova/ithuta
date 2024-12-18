7d13f9f9903eb63a026b513ebf87f18c50266976
Marcos Jova <marcosjova3@gmail.com>
2024-12-18 22:48:54 +0530

    ...

--- src/components/UpgradeButton.tsx
+++ src/components/UpgradeButton.tsx
@@ -37,7 +37,7 @@ const UpgradeButton = ({ isMobile }: UpgradeButtonProps) => {
       disabled={pending}
       className="w-full"
     >
-      Upgrade Now
+      Seja parte dos pros
     </Button>
   );
 };
